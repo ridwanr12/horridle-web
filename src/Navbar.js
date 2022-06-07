c1d3d569e6aed4259a564cbc0804338bdcd60f5e
Ridwan Raditya <ridwan.raditya01@gmail.com>
2022-06-07 17:39:37 +0700

    riddledetails

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
       <h1>HORRIDLE</h1>
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/create">New Riddle</Link>
+        <Link to="/signup">Sign Up</Link>
       </div>
     </nav>
   );
--- src/RiddleDetails.js
+++ src/RiddleDetails.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import useFetch from "./useFetch";
 
 const RiddleDetails = () => {
   const { id_riddle } = useParams();
@@ -45,16 +44,18 @@ const RiddleDetails = () => {
   // console.log(data);
   // console.log(data[0].values);
   
+  // console.log(data[0]);
   return (
-    <div className="horridle-details" key={id_riddle}>
+    <div className="apa" key={id_riddle}>
+      <button onClick={handleClick} className="back-arrow"></button>
       {isPending && <div>Loading ....</div>}
       {error && <div>{error}</div>}
       {data && (
-        <article>
+        <article className="horridle-details">
           <h1 className="title">{data[0].title}</h1>
           <br />
           <h2>Penulis dengan id : {data[0].id_user_author}</h2>
-          <p>Writter by {data[0].id_user_author} </p>
+          <p>Created at {data[0].date} </p>
           <div>{data[0].riddle_text} </div>
           <div>{data[0].riddle_text} </div>
           <button onClick={handleClick}>Back</button>
